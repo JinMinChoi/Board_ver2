fe2b8c173da16e016f56beb86bbfc4c7c4637d05
JinMinChoi <jinmin2216@naver.com>
2021-12-02 20:07:09 +0900

    feat: Commit first

---
+++ src/test/java/me/jinmin/boardver2/BoardVer2ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.jinmin.boardver2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardVer2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
